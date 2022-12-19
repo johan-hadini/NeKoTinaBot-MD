9eb1edba4775b6f0b801c2471eb00671ab7a6450
Yesenia_M <113145347+Yesenia57@users.noreply.github.com>
2022-12-19 13:28:32 -0500

    𝗕𝘆 𝗬𝗲𝘀𝗲𝗻𝗶𝗮
    𝗕𝘆 𝗬𝗲𝘀𝗲𝗻𝗶𝗮

---
+++ plugins/wa-grupos.js
@@ -0,0 +1,27 @@
+/**
+[ By @NeKosmic || https://github.com/NeKosmic/ ]
+**/
+let handler = async (m, { conn, args }) => {
+	let pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || './multimedia/imagenes/avatar_contact.png'
+	let name = await conn.getName(m.sender)
+try {
+	let wa = m.reply(`*🩸GRUPOS DEL BOT OFICIALES🍂*
+
+*GRUPO OFICIAL 1*
+
+1️⃣ https://chat.whatsapp.com/KkLE1VmVvP5L29k7qVaZ4t
+
+*_❤️ QUIERES AGREGAR UN GRUPO AL BOT CONTACTAME_*`)
+	conn.relayMessage(m.chat, {extendedTextMessage:{text: wa.enlaces+`\n${String.fromCharCode(8206).repeat(850)}ᴺᴼᵀᴬ" ˢᶦ ᵉⁿ ᵉˢᵗᵉ ᵍʳᵘᵖᵒ ᵉˢᵗᵃ́ ᵖʳᵒʰᶦᵇᶦᵈᵒ ᶜᵒᵐᵖᵃʳᵗᶦʳ ᵉⁿˡᵃᶜᵉˢ• ᴰᵉ ˢᵉʳ ⁿᵉᶜᵉˢᵃʳᶦᵒ ˡᵒˢ ᵃᵈᵐᶦⁿᶦˢᵗʳᵃᵈᵒʳᵉˢ ᵗᶦᵉⁿᵉⁿ ᵗᵒᵈᵒ ᵉˡ ᵈᵉʳᵉᶜʰᵒ ᵃ ᵉˡᶦᵐᶦⁿᵃʳ ᵃˡ ᵇᵒᵗ ⁻`, ...{ contextInfo:{"externalAdReply":{"title": `${name} Quieres agregar tu grupo :D?`,"body": `Toca aqui ;3`,"previewType": "PHOTO","thumbnailUrl": ``,"thumbnail": await getBuffer(pp), "sourceUrl": 'https://api.whatsapp.com/send?phone=51957041866&text=Wenasss!%0ADeseo%20agregar%20mi%20grupo%20al%20comando%20del%20bot%0AAsunto%20del%20grupo%3A%20%0ALink%3A%20'}}}
+}}, {})
+} catch (e) {
+m.reply(MultiNK.Error0())
+}
+}
+
+handler.help = ['wagrupos']
+handler.tags = ['']
+handler.command = /^(wagrupos|gruposwa|grupos)$/i
+handler.limit = 2
+
+export default handler
